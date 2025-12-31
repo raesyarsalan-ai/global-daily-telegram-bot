be44218b8274a27113745c40e2c9f7a59dcd2306
raesyarsalan-ai <raesy.arsalan@gmail.com>
2025-12-31 11:00:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,23 @@
-import asyncio
 from telegram.ext import Application, CommandHandler
 
 from config import BOT_TOKEN
-from database import init_db
 from handlers import (
     start_handler,
     add_task_handler,
     list_tasks_handler,
-    done_task_handler
+    done_task_handler,
 )
 
-
-async def main():
-    init_db()
-
+def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start_handler))
-    application.add_handler(CommandHandler("addtask", add_task_handler))
+    application.add_handler(CommandHandler("add", add_task_handler))
     application.add_handler(CommandHandler("tasks", list_tasks_handler))
-    application.add_handler(CommandHandler("donetask", done_task_handler))
+    application.add_handler(CommandHandler("done", done_task_handler))
 
-    await application.run_polling()
+    application.run_polling()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
