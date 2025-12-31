1c9daae7483c1cec3a57bf67124264fdd1018fc2
raesyarsalan-ai <raesy.arsalan@gmail.com>
2025-12-31 09:52:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import router
+from database import init_db
 
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
+
+    await init_db()
+
     dp.include_router(router)
     await dp.start_polling(bot)
 
