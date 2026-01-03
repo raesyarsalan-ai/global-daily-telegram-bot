dc7b0f2bee8263366144e926249b68173aeb944d
raesyarsalan-ai <raesy.arsalan@gmail.com>
2026-01-03 08:55:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,24 +7,21 @@ from telegram.ext import (
     filters,
 )
 from config import BOT_TOKEN
-from database import init_db
 import handlers
 
 
 def main():
-    init_db()
-
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", handlers.start))
     app.add_handler(CallbackQueryHandler(handlers.callback_handler))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handlers.text_handler))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, handlers.text_handler)
+    )
 
     print("🤖 Bot is running (callbacks enabled)...")
 
-    app.run_polling(
-        allowed_updates=Update.ALL_TYPES
-    )
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == "__main__":
