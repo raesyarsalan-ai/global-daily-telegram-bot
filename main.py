4f871f74577bc9e56dce26832a0ce7e042a14486
raesyarsalan-ai <raesy.arsalan@gmail.com>
2026-01-07 21:36:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    CallbackQueryHandler,
-)
+from telegram.ext import ApplicationBuilder, CommandHandler
 from config import BOT_TOKEN
 from database import init_db
 from handlers.start import start
-from handlers.admin import admin_panel
-
+from handlers.premium import premium_handler
 
 def main():
     init_db()
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("admin", admin_panel))
+    app.add_handler(CommandHandler("premium", premium_handler))
 
-    print("🤖 Bot is running securely...")
+    print("🤖 Bot running (Production)")
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
