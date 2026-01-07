9d2dbfb8a30c420c7962fc9f86a025e7d4aa11c9
raesyarsalan-ai <raesy.arsalan@gmail.com>
2026-01-07 21:03:05 +0330

    Delete weather.py

--- weather.py
+++
@@ -1,7 +0,0 @@
-import requests
-from config import WEATHER_API_KEY
-
-def weather_by_coords(lat, lon):
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units=metric"
-    r = requests.get(url).json()
-    return f"{r['weather'][0]['description']} | {r['main']['temp']}°C"
